78cde6c402ddb1ac4a9dfbe9cdcca766a2763c46
Isaiah Jenks <idjthesmart1@gmail.com>
2026-04-27 16:37:25 -0500

    Added comments to error_handler.cpp

--- src/backend/error_handler.cpp
+++ src/backend/error_handler.cpp
@@ -1,21 +1,30 @@
 #include "../../include/error_handler.hpp"
 
+/*
+Description: This file implements the CalculatorException class and the ErrorHandler class.
+The CalculatorException class contains error information for custom errors related to the calculator.
+Each exception instance includes the type of error, a message, and the position in the expression where the error occurred.
+The ErrorHandler class provides a static method to format error messages based on the CalculatorException instance passed to it.
+
+Input: This module takes an ErrorType (defined in error_types.hpp), a string with error details, and an optional unsigned integer position of an error.
+
+Output: A formatted error string.
+
+Algorithm: When a CalculatorException is created, the constructor initializes the error type, message, and position.
+The input message is changed and stored using the formatError method.
+*/
+
 CalculatorException::CalculatorException(ErrorType type, const std::string& message, std::size_t position)
     : type(type), position(position) {
     this->message = ErrorHandler::formatError(type, message, position);
 }
 
-const char* CalculatorException::what() const noexcept {
-    return message.c_str();
-}
+// Getter for the error message (formatted as an array of characters not std::string)
+const char* CalculatorException::what() const noexcept { return message.c_str(); }
 
-ErrorType CalculatorException::getType() const {
-    return type;
-}
+ErrorType CalculatorException::getType() const { return type; }
 
-std::size_t CalculatorException::getPosition() const {
-    return position;
-}
+std::size_t CalculatorException::getPosition() const { return position; }
 
 std::string ErrorHandler::formatError(ErrorType type, const std::string& details, std::size_t position) {
     std::string prefix;
